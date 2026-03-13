dbdf4b898bd8a1d8c8774d7850ee037b02fdc665
Lucjan Pucelak <lucjanpucelak@Lucjans-MacBook-Pro.local>
2026-03-13 17:41:28 +0100

    Add test for count_team_members

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-from base_program import format_greeting
+from base_program import format_greeting, count_team_members
 
 def run_tests():
     test_members = ["Lucjan Pucelak", "Szymon Lisowski", "Efran Fernandez", "Oskar Nowakowski"]
@@ -12,6 +12,12 @@ def run_tests():
         print("\nformat_greeting passed.")
     else:
         print("\nformat_greeting failed.")
+
+    # 2. Normal case for count_team_members
+    if count_team_members(test_members) == 4:
+        print("count_team_members normal case passed.")
+    else:
+        print("count_team_members normal case failed.")
 
 if __name__ == "__main__":
     run_tests()

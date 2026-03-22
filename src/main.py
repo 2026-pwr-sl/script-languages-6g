fe22a3c27d342c86c7b484191f40fb0e097acec3
oskar <o.nowakowski01@gmail.com>
2026-03-22 22:51:38 +0100

    Remove save_to_json call from main function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-from team import team_data, save_to_json
+from team import team_data
 from utils import greet_team, display_team_members
 
 
 def main():
     greet_team(team_data)
     display_team_members(team_data)
-    save_to_json(team_data, "data/team_data.json")
 
 
 if __name__ == "__main__":

69e873e1a53a7436c49ff19003b71b81f0cd01a5
oskar <o.nowakowski01@gmail.com>
2026-05-03 14:31:20 +0200

    Add HTTPRequest class, reqstr2obj function and test

--- src/lab7.py
+++ src/lab7.py
@@ -1,1 +1,26 @@
+import pytest
 
+
+class HTTPRequest:
+    def __init__(self, request_type, path, protocol):
+        self.request_type = request_type
+        self.path = path
+        self.protocol = protocol
+        
+        
+def reqstr2obj(request_string):
+    """Function gets text HTTP request and returns HTTP request object"""
+    pass
+
+
+def test_reqstr2obj_type_error():
+    with pytest.raises(TypeError):
+        reqstr2obj(123)
+        
+        
+def run():
+    test_reqstr2obj_type_error()
+    
+
+if __name__ == "__main__":
+    run()
